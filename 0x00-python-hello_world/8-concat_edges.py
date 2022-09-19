e4258aa5481a4594faf88d9500ac6e7deddc4008
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-19 16:09:02 +0100

    8 please

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:66], str[-22:-19], str[:7])
+str = f"{str[39:66]} {str[-22:-18]} {str[:7]}"
+print(str)

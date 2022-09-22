e932f20a7149c8cb16fb6a57fcd0cc9d683bdb0e
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-22 16:41:11 +0100

    tasks one to three

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-list_of_args = []
+    list_of_args = []
 
-if len(argv) == 1:
-    print("0")
-else:
-    for i in range(1, len(argv)):
-        list_of_args.append(int(argv[i]))
-        
-    print(sum(list_of_args))
+    if len(argv) == 1:
+        print("0")
+    else:
+        for i in range(1, len(argv)):
+            list_of_args.append(int(argv[i]))
+
+        print(sum(list_of_args))

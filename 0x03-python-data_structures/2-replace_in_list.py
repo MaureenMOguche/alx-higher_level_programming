b8d0e8eea00840b6407da95a66af11b4ff64cfc3
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-24 22:11:34 +0100

    seven

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,5 @@ def replace_in_list(my_list, idx, element):
     if 0 > idx > len(my_list):
         return my_list
     else:
-        my_list[idx] = element
+        my_list.insert(idx, element)
         return my_list
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    () =
+    if len(tuple_a) < 2:
+        if len(tuple_a) < 1:
+            tuple_a = tuple_a + (0, 0,)
+        else:
+            tuple_a = tuple_a + (0,)
+    if len(tuple_b) < 2:
+        if len(tuple_b) < 2:
+            if len(tuple_b) < 1:
+                tuple_b = tuple_b + (0, 0,)
+            else:
+                tuple_b = tuple_b + (0,)
+
+    x, y = tuple_a
+    i, j = tuple_b
+
     new_tuple = ()
-    for i in tuple_a:
-        new_tuple[i] = tuple_a[i] + tuple_b[i]
+
+    for u in range(len(tuple_a)):
+        new_tuple = x + i, y + j
     return new_tuple

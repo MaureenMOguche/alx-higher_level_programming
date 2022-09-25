d652e2aa75b59fa3bb9e1f76d162bdf113967b3b
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-25 20:11:34 +0100

    some tasks

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        my_list[idx] = element
+        del my_list[idx]
         return my_list

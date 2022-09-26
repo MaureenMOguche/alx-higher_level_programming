fdc5c327879a16557cd92fc6ded12dc1aaa3bc6f
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-26 23:54:58 +0100

    eight

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if len(sentence) == 0:
         return (0, None)
     else:
         return (len(sentence), sentence[0])

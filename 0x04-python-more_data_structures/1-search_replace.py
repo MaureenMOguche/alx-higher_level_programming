fd5e03f533e81614cb83842142cb3a985e8ce2e8
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-29 03:45:22 +0100

    one

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list is None:
+        return my_list
+
     new_list = my_list.copy()
     for i in range(len(my_list) - 1):
         if new_list[i] == search:

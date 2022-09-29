0e6b417de001bdf122a3c146062765b507b68636
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-29 03:47:22 +0100

    one

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is None:
-        return my_list
-
     new_list = my_list.copy()
-    for i in range(len(my_list) - 1):
-        if new_list[i] == search:
+    for i, num in enumerate(my_list):
+        if num == search:
             new_list[i] = replace
 
     return new_list

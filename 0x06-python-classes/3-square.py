65e76771c16ebd4d08c038e62a89e9f8a077ee73
MaureenMOguche <career.oguchemaureen@gmail.com>
2022-10-12 00:28:52 +0100

    three

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""defines a square object"""
 
 
 class Square:
@@ -23,3 +24,7 @@ class Square:
                 self.__size = size
         else:
             raise TypeError("size must be an integer")
+
+    def area(self):
+        areasize = self.__size * self.__size
+        return areasize

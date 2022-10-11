3c97d14c74075630db531fac397118159fe55000
MaureenMOguche <career.oguchemaureen@gmail.com>
2022-10-11 01:48:37 +0100

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             count += 1
         except (IndexError, TypeError):
-            break;
+            break
 
     print()
     return count

98a25ca19c8a4b9882994d96bbf081b9a5f3816d
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-12-01 19:18:54 +0100

    elev

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 --lists all shows in the database
 SELECT s.title, g.genre_id
-FROM tv_shows AS s, tv_show_genres AS g
-WHERE tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+	FROM tv_shows AS s
+		LEFT JOIN tv_show_genres AS g
+		ON s.id = g.show_id
+	ORDER BY tv_shows.title, tv_show_genres.genre_id

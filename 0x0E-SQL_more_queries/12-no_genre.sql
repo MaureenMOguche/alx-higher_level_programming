5d6d5080f40eaed389b6c42c2628f06985125879
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-12-01 20:00:29 +0100

    11, 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,5 +4,5 @@ SELECT s.title, g.genre_id
 	FROM tv_shows AS s
 		LEFT JOIN tv_show_genres AS g
 		ON s.id = g.show_id
-	WHERE g.genre_id = NULL
+	WHERE g.genre_id IS NULL
 	ORDER BY s.title, g.genre_id;

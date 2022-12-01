3f6891ab0f89f8fa8a6d162d593745254a1f204c
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-12-01 20:02:08 +0100

    11, 12

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 --lists all shows in the database
 SELECT s.title, g.genre_id
-	FROM tv_shows AS s, tv_show_genres AS g
-	WHERE s.id = g.show_id
+	FROM tv_shows AS s
+		LEFT JOIN tv_show_genres AS g
+		ON s.id = g.show_id
 	ORDER BY s.title, g.genre_id;

6386d42e2c89b50c90c5d79d6efcb9e745ced3ae
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-12-01 18:25:51 +0100

    eight

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all the cities of california that can be founD
-SELECT name FROM hbtn_0d_usa.cities
+SELECT id, name FROM cities
 WHERE state_id = 
-	(SELECT id FROM hbtn_0d_usa.states
+	(SELECT id FROM states
 		WHERE name = 'California');

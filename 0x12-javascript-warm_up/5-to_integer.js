902a3080155570fb8f1b75bec789275be14c543c
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-12-19 13:16:41 +0100

    task5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+
+if (isNaN(args[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ', args[2]);
+}

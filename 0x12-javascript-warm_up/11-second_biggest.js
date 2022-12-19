c406022a9323045110b1e924c6f4e598cf847539
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-12-19 16:09:25 +0100

    eleven edit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
+let arr = [];
 
-arr = [];
 if (args.length <= 3) {
   console.log(0);
-} else {
-  for (let i = 2; i < args.length; i++)
-  {
-	  arr.push(args[i]);
+}
+else {
+  for (let i = 2; i < args.length; i++) {
+    arr.push(args[i]);
   }
   arr.sort();
   console.log(arr[arr.length - 2]);
